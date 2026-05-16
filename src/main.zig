5e376a442118077f8dee14723dcb3e1e298cc85e
BradenEverson <bradeneverson@gmail.com>
2026-05-15 20:17:35 -0500

    Init

---
+++ src/main.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+const Io = std.Io;
+
+const umi = @import("umi");
+
+pub fn main(init: std.process.Init) !void {
+    _ = init;
+}
---
+++ src/root.zig
@@ -0,0 +1,4 @@
+//! The core Umi language spec :)
+
+const std = @import("std");
+const Io = std.Io;
