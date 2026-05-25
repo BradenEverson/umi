7be106a8fe64fc222e49e7b5b662bd19edffc7a7
BradenEverson <bradeneverson@gmail.com>
2026-05-25 11:02:46 -0500

    Start off with an interpreter

--- src/root.zig
+++ src/root.zig
@@ -9,6 +9,7 @@ pub const compiler = @import("compiler.zig");
 pub const semantic_analysis = @import("semantic_analysis.zig");
 pub const types = @import("type.zig");
 pub const optimizer = @import("optimizer.zig");
+pub const interpreter = @import("interp.zig");
 
 test {
     _ = @import("compiler.zig");
@@ -19,4 +20,5 @@ test {
     _ = @import("semantic_analysis.zig");
     _ = @import("vm.zig");
     _ = @import("type.zig");
+    _ = @import("interp.zig");
 }
