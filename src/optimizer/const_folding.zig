9b85082431d4cd06e11bf65a5885a858ef419b16
BradenEverson <bradeneverson@gmail.com>
2026-05-25 17:24:07 -0500

    Half bake constant folding before realizing that we need the type checker to also validate binary and unary operations

--- src/optimizer/const_folding.zig
+++ src/optimizer/const_folding.zig
@@ -5,10 +5,48 @@ const Allocator = std.mem.Allocator;
 
 const parser = @import("../parser.zig");
 const TopLevel = parser.TopLevel;
+const Expr = parser.Expr;
 
 const optimizer = @import("../optimizer.zig");
 
-pub fn foldConsts(alloc: Allocator, tl: *TopLevel) optimizer.OptimizeError!void {
+pub fn foldConsts(
+    alloc: Allocator,
+    tl: *TopLevel,
+) optimizer.OptimizeError!void {
     _ = alloc;
     _ = tl;
+}
+
+pub fn comptimeEval(
+    alloc: Allocator,
+    expr: *Expr,
+) optimizer.OptimizeError!void {
+    switch (expr.*) {
+        .binary_op => |b| {
+            if (b.left.* == .literal and
+                b.right.* == .literal)
+            {
+                const newVal = comptimeBinOp(
+                    alloc,
+                    b.left.literal,
+                    b.right.literal,
+                    b.op,
+                );
+                _ = newVal;
+            }
+        },
+        else => {},
+    }
+}
+
+pub fn comptimeBinOp(
+    alloc: Allocator,
+    left: parser.Literal,
+    right: parser.Literal,
+    op: parser.BinaryOp,
+) optimizer.OptimizeError!*Expr {
+    _ = alloc;
+    _ = left;
+    _ = right;
+    _ = op;
 }
