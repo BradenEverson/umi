40eaafb3f1005bb9c2041c4d2fbb98e84ecf2134
BradenEverson <bradeneverson@gmail.com>
2026-05-29 05:11:32 -0500

    Begin putting arch specific stuff in compiler, maybe it should go in an arch file

--- src/compiler.zig
+++ src/compiler.zig
@@ -1,3 +1,48 @@
 //! Translating our optimized IR into a target,
 //! possibly LLVM IR or raw machine code based
 //! on the target set here
+
+pub const Arch = enum {
+    x86_64,
+};
+
+/// How many function parameters go to registers
+/// before spilling to the stack
+pub fn paramConvention(arch: Arch) usize {
+    return switch (arch) {
+        .x86_64 => 6,
+    };
+}
+
+/// Word size by architecture
+pub fn wordSize(arch: Arch) usize {
+    return switch (arch) {
+        .x86_64 => 8,
+    };
+}
+
+const X86_64_REGISTERS: [][]const u8 = &[_][]const u8{
+    "rax",
+    "rbx",
+    "rcx",
+    "rdx",
+    "rsi",
+    "rdi",
+    "rbp",
+    "rsp",
+    "r8",
+    "r9",
+    "r10",
+    "r11",
+    "r12",
+    "r13",
+    "r14",
+    "r15",
+};
+
+/// Register count and names by architecture
+pub fn registers(arch: Arch) [][]const u8 {
+    return switch (arch) {
+        .x86_64 => X86_64_REGISTERS,
+    };
+}
--- src/reg_alloc.zig
+++ src/reg_alloc.zig
@@ -3,16 +3,35 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
+const builtin = @import("builtin");
+
 const ir = @import("ir.zig");
 const IrTopLevel = ir.IrTopLevel;
 const FunctionIR = ir.FunctionIR;
 const ThreeAddressCode = ir.ThreeAddressCode;
 const Operand = ir.Operand;
 
+const compiler = @import("compiler.zig");
+
 fn dependsOn(code: ThreeAddressCode, op: Operand) bool {
     return std.mem.eql(Operand, code.arg1, op) or
         std.mem.eql(Operand, code.arg2, op);
 }
+
+pub const StackFrame = struct {
+    /// Holds the stack offset to all local and parameter
+    /// variables
+    variables: std.StringHashMapUnmanaged(usize) = .{},
+    size: usize = 0,
+
+    pub fn deinit(sf: *StackFrame, alloc: Allocator) void {
+        sf.variables.deinit(alloc);
+    }
+};
+
+pub const AllocatedFunction = struct {
+    layout: StackFrame = .{},
+};
 
 pub const RegAllocError = Allocator.Error;
 
