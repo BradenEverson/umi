cadb705700b85b4c0410b2f1b6e41212faa759cd
BradenEverson <bradeneverson@gmail.com>
2026-06-01 05:46:51 -0500

    Basic block structure

--- src/ir/cfg.zig
+++ src/ir/cfg.zig
@@ -1,3 +1,23 @@
 //! Control Flow Graph for later IR passes
 
-pub const BasicBlock = struct {};
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const ir = @import("../ir.zig");
+const TAC = ir.ThreeAddressCode;
+
+pub const BasicBlock = struct {
+    /// A slice into the existing TAC stream
+    instructions: []TAC = &[0]TAC{},
+
+    // todo: Might need connection type too
+    // like conditional etc
+    connections: std.ArrayList(usize) =
+        .empty,
+
+    pub fn deinit(bb: *BasicBlock, alloc: Allocator) void {
+        bb.connections.deinit(alloc);
+    }
+};
+
+blocks: std.ArrayList(BasicBlock) = .empty,
